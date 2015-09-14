4dbe2174b9e7a58df78724ab48a0c1f9822a8190
dgonzalez870 <dgonzalez870@gmail.com>
2015-09-13 20:42:12 -0430

    HolaAndroid básico

---
+++ app/src/main/java/com/prueba/holaAndroid/MainActivity.java
@@ -0,0 +1,15 @@
+package com.prueba.holaAndroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by JARAMILLO on 13/09/2015.
+ */
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_main);
+    }
+}
