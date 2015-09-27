5cbd71a39df36149cb4635415d5cab1b11eb4bda
Dennys Gonzalez <dgonzalez870@yahoo.com>
2015-09-27 15:25:16 -0430

    Merge branch 'master' of https://github.com/dgonzalez870/HolaAndroid into Idiomas
    Conflicts:
    	.idea/misc.xml

--- app/src/main/java/com/prueba/holaAndroid/MainActivity.java
+++ app/src/main/java/com/prueba/holaAndroid/MainActivity.java
@@ -1,7 +1,11 @@
 package com.prueba.holaAndroid;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity{
     @Override
@@ -9,4 +13,15 @@ public class MainActivity extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_main);
     }
-}
+
+    public void saludar(View v){
+        Toast toast=Toast.makeText(this, R.string.string_saludo, Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.show();
+    }
+
+    public void iniciarSaludo(View v){
+        Intent intent=new Intent(this,SaludoActivity.class);
+        startActivity(intent);
+    }
+ }
---
+++ app/src/main/java/com/prueba/holaAndroid/SaludoActivity.java
@@ -0,0 +1,11 @@
+package com.prueba.holaAndroid;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SaludoActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_saludo);
+    }
+}
